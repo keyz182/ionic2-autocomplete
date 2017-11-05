d448f691e015b69659dd83dbea88a2e967b23e6d
Kieran David Evans <keyz182@gmail.com>
2017-11-05 12:44:26 +0000

    handle null or blank value

--- src/boldprefix.pipe.ts
+++ src/boldprefix.pipe.ts
@@ -10,6 +10,7 @@ import { Injectable, PipeTransform, Pipe } from '@angular/core';
 export class BoldPrefix implements PipeTransform {
     transform(value: string, keyword: string): any {
         if (!keyword) return value;
+        if (!value) return value;
         let escaped_keyword = keyword.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
         return value.replace(new RegExp(escaped_keyword, 'gi'), function(str) { return str.bold(); });
     }
